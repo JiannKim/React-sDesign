8d8edf4a8196a87e53c02a8385bfdd1832fc37d6
Dominica <dominica@Dominicaui-MacBookPro.local>
2020-08-25 15:25:41 +0900

    header 반응형까지

--- sdesign-client/src/components/app.header.js
+++ sdesign-client/src/components/app.header.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import '../style/app.header.css';
+import { BsSearch } from 'react-icons/bs';
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
 function Header() {
   return (
     <div>
       <header className="App-header">
+        <div className="Tab-wrapper">
+          <button className="Tab-app-menu"><HiOutlineMenuAlt4 /></button>
           <a href="/">
-            <img className="App-logo" src="https://i.ibb.co/3y3zRr3/s-Design-Logo-1.png" alt="s-Design-Logo-1" border="0" />
+            <img className="App-logo" src="https://i.ibb.co/FVRcqBh/s-Design-Logo.png" alt="s-Design-Logo" />
           </a>
+          <button href="/" className="Tab-icon-BeSearch"><BsSearch /></button>
+        </div>
           <div className="search-input">
-            <img className="search-icon" src="https://i.ibb.co/5LmPhyS/serch-Icon.png" alt="serch-Icon" border="0" />
+            <img className="search-icon" src="https://i.ibb.co/5LmPhyS/serch-Icon.png" alt="serch-Icon" />
             <input type="text" placeholder="Search for sound effects"></input>
           </div>
           <div className="App-menu">
--- sdesign-client/src/components/app.homeHeader.js
+++ sdesign-client/src/components/app.homeHeader.js
@@ -4,10 +4,16 @@ import '../style/app.homeHeader.css';
 function HomeHeader() {
     return(
         <div className="Home-header" >
-            {/* <img src="https://i.ibb.co/p6HBSw4/denisse-leon-OVEWb-Igff-Dk-unsplash.jpg" alt="denisse-leon-OVEWb-Igff-Dk-unsplash" width="5184" height="3456" data-load="full" style=""/> */}
-            <img className="Home-mainImg" src="https://i.ibb.co/LDTX3R9/denisse-leon-OVEWb-Igff-Dk-unsplash.jpg" alt="denisse-leon-OVEWb-Igff-Dk-unsplash" />
-            {/* <img className="Home-mainImg" src="https://i.ibb.co/fYR6vW6/Home-main-Image.png" alt="Home-main-Image" /> */}
-            {/* <img className="Home-mainImg" src="https://i.ibb.co/t4tLvW1/Home-main-Image.png" alt="Home-main-Image" /> */}
+            <div className="Home-contents">
+                <div className="Home-mainImg"></div>
+                <div className="Home-mainContents">
+                    <div className="mainText">
+                        <h1>완벽한 사운드를 찾아보세요.</h1>
+                        <p>매주 업데이트되는 280,000 개 이상의 음향효과 라이브러리를 무제한 다운로드하세요.</p> 
+                        <button>음향효과 듣기</button>                       
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }

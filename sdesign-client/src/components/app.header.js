17c958043814c3e07d658f39bea8447c06b424a4
Dominica <dominica@Dominicaui-MacBookPro.local>
2020-08-27 17:02:22 +0900

    search toggle working

--- sdesign-client/src/components/app.header.js
+++ sdesign-client/src/components/app.header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import '../style/app.header.css';
 import '../style/menu.motion.css';
+import '../style/search.motion.css';
 import { BsSearch } from 'react-icons/bs';
-import { HiOutlineMenuAlt4 } from "react-icons/hi";
 
 function Header() {
   return (
     <div>
       <header className="App-header">
         <div className="Tab-wrapper">
-          <div id="menuToggle">
+          <nav id="menuToggle">
             {/* <!--
             A fake / hidden checkbox is used as click reciever,
             so you can use the :checked selector on it.
@@ -23,7 +23,8 @@ function Header() {
             not that McDonalds stuff.
             --> */}
             <span></span>
-            <span id="span"></span>
+            {/* 가운데스펜 없는척 */}
+            <span id="hiddenSpan"></span>
             <span></span>
             
             {/* <!--
@@ -37,23 +38,36 @@ function Header() {
               <a href="#"><li>Login</li></a>
               <a href="/"><li>Register</li></a>
             </ul>
-          </div>
-          {/* <button className="Tab-app-menu"><HiOutlineMenuAlt4 /></button> */}
+          </nav>
           <a href="/">
             <img className="App-logo" src="https://i.ibb.co/FVRcqBh/s-Design-Logo.png" alt="s-Design-Logo" />
           </a>
-          <button href="/" className="Tab-icon-BeSearch"><BsSearch /></button>
+          <div id="searchToggle">
+            <input type="checkbox" className="search-bnt" />
+            <BsSearch className="Tab-icon-BeSearch" />
+            
+            <div id="search">
+              <img src="https://i.ibb.co/5LmPhyS/serch-Icon.png" alt="serch-Icon" />
+              <input type="text" placeholder="Search for sound effects" />
+            </div>
+          </div>
+          {/* <button href="/" className="Tab-icon-BeSearch"><BsSearch /></button> */}
         </div>
-          <div className="search-input">
-            <img className="search-icon" src="https://i.ibb.co/5LmPhyS/serch-Icon.png" alt="serch-Icon" />
-            <input type="text" placeholder="Search for sound effects"></input>
-          </div>
-          <div className="App-menu">
-            <a href="/" target="blank" className="header-menu">Pricing</a>
-            <a href="/" target="blank" className="header-menu">FAQ</a>
+        <div className="search-input">
+          <img className="search-icon" src="https://i.ibb.co/5LmPhyS/serch-Icon.png" alt="serch-Icon" />
+          <input type="text" placeholder="Search for sound effects"></input>
+        </div>
+        <nav>
+          <ul className="App-menu">
+            <a href="/" target="blank" className="header-menu"><li>Pricing</li></a>
+            <a href="/" target="blank" className="header-menu"><li>FAQ</li></a>
+            <a href="/" target="blank" className="header-menu"><li>SFXHD</li></a>
+            <a href="/" target="blank" className="header-menu"><li>Login/Register</li></a>
+            {/* <a href="/" target="blank" className="header-menu">FAQ</a>
             <a href="/" target="blank" className="header-menu">SFXHD</a>
-            <a href="/" target="blank" className="header-menu">Login/Register</a>
-          </div>
+            <a href="/" target="blank" className="header-menu">Login/Register</a> */}
+          </ul>
+        </nav>
       </header>
     </div>
   );
